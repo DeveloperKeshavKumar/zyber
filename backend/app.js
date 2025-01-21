9b0bc50de907e87b4c5aa4094720edb326abf07d
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-22 00:46:46 +0530

    add cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const router = require('./routes/all.routes')
 const app = express();
 
 connectDB();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', router);
