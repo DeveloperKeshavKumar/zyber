969ede056959cc6498197040b28b7abd06da95af
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-21 23:46:12 +0530

    configure app for json data

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./config/db');
+const router = require('./routes/all.routes')
 const app = express();
-app.use(cors());
 
-app.get('/', (req, res) => {
-   res.send('Hello World');
-})
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router);
 
 module.exports = app;
