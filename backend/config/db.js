94efe5bf6945baef63f727570b36e889d9a106ca
Keshav Kumar <94639526+DeveloperKeshavKumar@users.noreply.github.com>
2025-01-24 19:56:39 +0530

    Add db uri on logs

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectDB() {
    mongoose.connect(process.env.MONGO_URI)
-   .then(() => console.log('Connected to MongoDB'))
+   .then(() => console.log('Connected to MongoDB '+ process.env.MONGO_URI))
    .catch(err => console.log(err));
 }
 
