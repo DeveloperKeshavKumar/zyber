b8443e93eb8ab8f186ff1d1e933b4ba3991eab00
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-25 22:46:11 +0530

    import captain routes to main router

--- backend/routes/all.routes.js
+++ backend/routes/all.routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userRoutes = require('./user.routes');
+const captainRoutes = require('./captain.routes');
 
 router.use('/users', userRoutes);
+router.use('/captain', captainRoutes);
 
 router.get('/', (req, res) => {
    res.send('Server is spinning up!');
