a0f614f682c4327760f79b3ad9f60a6c4b0974f4
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-26 23:58:36 +0530

    implement rest of captain auth routes

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,27 +1,64 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const blacklistTokenModel = require('../models/blacklistTokens.model');
+const blackListTokenModel = require('../models/blackListToken.model');
+const captainModel = require('../models/captain.model');
 
-module.exports.authenticateUser = async (req, res, next) => {
-   const token = req.cookies.token || req.headers.authorization?.replace('Bearer ', '');
+
+module.exports.authUser = async (req, res, next) => {
+   const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
    if (!token) {
       return res.status(401).json({ message: 'Unauthorized' });
    }
 
-   const isBlacklisted = await blacklistTokenModel.findOne({ token: token });
+
+   const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+   if (isBlacklisted) {
+      return res.status(401).json({ message: 'Unauthorized' });
+   }
+
+   try {
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await userModel.findById(decoded._id)
+
+      req.user = user;
+
+      return next();
+
+   } catch (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+   }
+}
+
+module.exports.authCaptain = async (req, res, next) => {
+   const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+   console.log(token);
+
+   if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+   }
+
+   const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+   console.log(isBlacklisted);
+
    if (isBlacklisted) {
       return res.status(401).json({ message: 'Unauthorized' });
    }
 
    try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await userModel.findById(decoded._id);
+      const captain = await captainModel.findById(decoded._id)
+      req.captain = captain;
 
-      req.user = user;
+      return next()
+   } catch (err) {
+      console.log(err);
 
-      return next();
-   } catch (error) {
-      return res.status(401).json({ message: 'Unauthorized. Try again later' });
+      res.status(401).json({ message: 'Unauthorized' });
    }
 }
