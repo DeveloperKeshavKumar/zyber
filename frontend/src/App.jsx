b744db9cf379de38f229dfaf48cfd88500a2914f
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-28 23:45:35 +0530

    add captain auth routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
+import Captainlogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
 
 const App = () => {
 
@@ -11,6 +14,8 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
       </Routes>
     </div>
   )
