996275bb1222085963aecf9495a5ba5567efba88
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-01-30 21:07:43 +0530

    add axios and implement map service

--- backend/routes/all.routes.js
+++ backend/routes/all.routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const userRoutes = require('./user.routes');
 const captainRoutes = require('./captain.routes');
+const mapRoutes = require('./map.routes');
 
 router.use('/users', userRoutes);
 router.use('/captain', captainRoutes);
+router.use('/map', mapRoutes);
 
 router.get('/', (req, res) => {
    res.send('Server is spinning up!');
