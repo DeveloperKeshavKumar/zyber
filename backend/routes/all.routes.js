626a40e9fac3e57120833089fd2b9d9d379b191b
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-02-01 23:07:54 +0530

    add ride routes to main router

--- backend/routes/all.routes.js
+++ backend/routes/all.routes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const userRoutes = require('./user.routes');
 const captainRoutes = require('./captain.routes');
 const mapRoutes = require('./map.routes');
+const rideRoutes = require('./ride.routes')
 
 router.use('/users', userRoutes);
 router.use('/captain', captainRoutes);
 router.use('/map', mapRoutes);
+router.use('/rides', rideRoutes);
 
 router.get('/', (req, res) => {
    res.send('Server is spinning up!');
