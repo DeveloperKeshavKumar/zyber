58e45ec29884a32f9d34712febf5121d4fce42e2
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-02-02 22:07:19 +0530

    add captain homepage

--- frontend/src/pages/CaptainProtectWrapper.jsx
+++ frontend/src/pages/CaptainProtectWrapper.jsx
@@ -1,55 +1,67 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { CaptainDataContext } from '../context/CapatainContext'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+import RidePopUp from '../components/RidePopUp'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import ConfirmRidePopUp from '../components/ConfirmRidePopUp'
 
-const CaptainProtectWrapper = ({
-   children
-}) => {
+const CaptainHome = () => {
 
-   const token = localStorage.getItem('token')
-   const navigate = useNavigate()
-   const { captain, setCaptain } = useContext(CaptainDataContext)
-   const [isLoading, setIsLoading] = useState(true)
+   const [ridePopupPanel, setRidePopupPanel] = useState(true)
+   const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false)
 
+   const ridePopupPanelRef = useRef(null)
+   const confirmRidePopupPanelRef = useRef(null)
 
 
-
-   useEffect(() => {
-      if (!token) {
-         navigate('/captain-login')
+   useGSAP(function () {
+      if (ridePopupPanel) {
+         gsap.to(ridePopupPanelRef.current, {
+            transform: 'translateY(0)'
+         })
+      } else {
+         gsap.to(ridePopupPanelRef.current, {
+            transform: 'translateY(100%)'
+         })
       }
-   }, [token])
+   }, [ridePopupPanel])
 
-   axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
-      headers: {
-         Authorization: `Bearer ${token}`
+   useGSAP(function () {
+      if (confirmRidePopupPanel) {
+         gsap.to(confirmRidePopupPanelRef.current, {
+            transform: 'translateY(0)'
+         })
+      } else {
+         gsap.to(confirmRidePopupPanelRef.current, {
+            transform: 'translateY(100%)'
+         })
       }
-   }).then(response => {
-      if (response.status === 200) {
-         setCaptain(response.data.captain)
-         setIsLoading(false)
-      }
-   })
-      .catch(err => {
-         console.log(err)
-         localStorage.removeItem('token')
-         navigate('/captain-login')
-      })
-
-   if (isLoading) {
-      return (
-         <div>Loading...</div>
-      )
-   }
-
-
+   }, [confirmRidePopupPanel])
 
    return (
-      <>
-         {children}
-      </>
+      <div className='h-screen'>
+         <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+            <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <Link to='/captain-home' className=' h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+               <i className="text-lg font-medium ri-logout-box-r-line"></i>
+            </Link>
+         </div>
+         <div className='h-3/5'>
+            <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+
+         </div>
+         <div className='h-2/5 p-6'>
+            <CaptainDetails />
+         </div>
+         <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+            <RidePopUp setRidePopupPanel={setRidePopupPanel} setConfirmRidePopupPanel={setConfirmRidePopupPanel} />
+         </div>
+         <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+            <ConfirmRidePopUp setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
+         </div>
+      </div>
    )
 }
 
-export default CaptainProtectWrapper
+export default CaptainHome
