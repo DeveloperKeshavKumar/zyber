8864ee08c13ebd15bbe70d7f7705a894efa8fcf9
Keshav Kumar <aboutkeshavkumar@hotmail.com>
2025-02-05 22:12:28 +0530

    add socket to server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const app = require('./app');
+const { initializeSocket } = require('./socket');
 
 const server = http.createServer(app);
+initializeSocket(server);
+
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
    console.log(`Server is running at port http://localhost:${process.env.PORT}`);
